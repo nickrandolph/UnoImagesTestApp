102e492be09bdebff827bd79e1ae9166b4e30010
Nick Randolph <nick@builttoroam.com>
2023-06-05 14:06:01 +1000

    chore: Added loaded/failed debug writing

--- ImagesTestApp/MainPage.xaml.cs
+++ ImagesTestApp/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,16 @@ namespace ImagesTestApp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ImageLoaded(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Image loaded - {((sender as Image)!.Source as BitmapImage)?.UriSource}");
+        }
+
+        private void ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Image failed to load - {((sender as Image)!.Source as BitmapImage)?.UriSource}");
         }
     }
 }
